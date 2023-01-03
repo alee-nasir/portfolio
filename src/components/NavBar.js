2088dc29eb5f0e451fd00a10142d32ee9266c26b
Ali Nasir <alee.nasir254@gmail.com>
2023-01-03 09:30:08 +0500

    navbar completed

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../assets/logo.png";
+import "../components/NavBar.css";
+
+const NavBar = () => {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container className="mt-2 mb-2">
+        <Navbar.Brand href="#home">
+          <img className="logo" src={logo} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          className="justify-content-center"
+          id="basic-navbar-nav"
+        >
+          <Nav className="navlink">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#aboutme">About Me</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#mywork">My Work</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
